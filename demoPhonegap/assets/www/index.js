39715dd512efdf3adf2a22f34868efff07df4a71
Du Huynh <huynhduyhaidu@gmail.com>
2014-01-19 02:09:28 +0700

    demo

---
+++ demoPhonegap/assets/www/index.js
@@ -0,0 +1,16 @@
+﻿(function() {
+    "use strict";
+
+    var MyApp = window.MyApp = { };
+    
+    // Uncomment the line below to disable platform-specific look and feel and to use the Generic theme for all devices
+     DevExpress.devices.current({ platform: "androidPhone" });
+
+    $(function() {
+        MyApp.app = new DevExpress.framework.html.HtmlApplication({
+            namespace: MyApp,});
+        MyApp.app.router.register(":view", { view: "home" });
+        MyApp.app.navigate();
+    });
+    
+})();
