24cbdcf661cfaf9c3f6ff53567d5a6160e9506a4
Du Huynh <huynhduyhaidu@gmail.com>
2014-01-19 03:25:49 +0700

    demo

---
+++ demoPhonegap/assets/www/index.js
@@ -0,0 +1,42 @@
+﻿(function() {
+    "use strict";
+
+    var MyApp = window.MyApp = { };
+    
+    // Uncomment the line below to disable platform-specific look and feel and to use the Generic theme for all devices
+    DevExpress.devices.current({ platform: "androidPhone" });
+
+    $(function() {
+        MyApp.app = new DevExpress.framework.html.HtmlApplication({
+            namespace: MyApp,
+            
+            navigationType: "navbar",
+            navigation: [
+              {
+                title: "Home",
+                action: "#home",
+                icon: "home"
+              },
+              {
+                title: "About",
+                action: "#about",
+                icon: "info"
+              }
+            ]
+        });
+        
+        MyApp.app.router.register(":view", { view: "home" });
+        MyApp.app.navigate();
+        AppNamespace.home = function() {
+			var viewModel = {
+				message : ko.observable('Welcome!'),
+				name : ko.observable(''),
+				sayHello : function() {
+					this.message("Hello " + this.name() + '!');
+				}
+			};
+			return viewModel;
+		};
+    });
+    
+})();
